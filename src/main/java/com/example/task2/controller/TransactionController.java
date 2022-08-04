9a4d304e03aa77ed5ac1c943531f42b4bff96640
AbdyrovElnar <eabdyrov@gmail.com>
2022-08-04 13:24:18 +0600

    created service and controller

---
+++ src/main/java/com/example/task2/controller/TransactionController.java
@@ -0,0 +1,11 @@
+package com.example.task2.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/transaction")
+@RequiredArgsConstructor
+public class TransactionController {
+}
